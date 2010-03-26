a1a61a5f2782af5736e84644e7c8f39592b880e3
Jonathan Meiss <jonathan@meiss.fr>
2010-03-26 15:21:42 +0100

    script de migration pour migrer les tables : annonces, offres et hebergements vers la nouvelle version du schema de la DB

--- db/schema.rb
+++ db/schema.rb
@@ -9,43 +9,47 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100325164621) do
+ActiveRecord::Schema.define(:version => 20100326135518) do
 
-  create_table "annonces", :force => true do |t|
+  create_table "ad", :force => true do |t|
     t.string   "reference"
     t.string   "title"
     t.text     "description"
     t.boolean  "is_validated"
-    t.integer  "hebergement_id"
+    t.integer  "housing_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "ad_id"
   end
 
-  create_table "hebergements", :force => true do |t|
-    t.integer  "hebergement_id"
-    t.integer  "offre_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "offres", :force => true do |t|
-    t.integer  "hebergement_id"
+  create_table "housing", :force => true do |t|
     t.integer  "address_id"
     t.integer  "user_id"
     t.string   "phone_contact"
     t.string   "email"
     t.string   "website"
     t.string   "name"
-    t.string   "is_visible"
     t.string   "type"
     t.string   "url_alias"
     t.string   "distance_commerce"
     t.string   "distance_train"
     t.string   "distance_transport"
-    t.string   "gmap_latitude"
-    t.string   "gmap_longitude"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "housing_id"
+    t.boolean  "has_elevator"
+    t.integer  "floor"
+    t.integer  "capacity"
+    t.boolean  "is_visible"
+  end
+
+  create_table "offer", :force => true do |t|
+    t.integer  "offer_id"
+    t.integer  "housing_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "price_id"
+    t.integer  "calendar_id"
   end
 
 end
