7190ccbadcecabe14c29792888a946881eac319c
jmeiss <jonathan@meiss.fr>
2010-05-08 00:50:35 +0200

    ajout de la barre lorsque le user est authent, a finir

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 
-User.create :email => 'admin@resasystem.com', :password => 'toto42'
+User.create :email => 'admin@resasystem.com', :password => 'toto42', :first_name => 'Jonathan', :last_name => 'Meiss'
