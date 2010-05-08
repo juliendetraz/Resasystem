0fa0afc841917ff1c5c43b167214cb744a765a35
Jonathan Meiss <jonathan@meiss.fr>
2010-05-08 17:03:47 +0200

    gestion du multilingue. Chie encore un peu mais je push pour MEP pour Romain

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def locale_switch_tag options
+    locale = options[:locale]
+    classes = ["langItem"]
+    classes << "currentLang" if locale == I18n.locale
+    content_tag :li, :class => classes.join(" ") do
+      link_to "<span id=\"lang_#{locale}\"><span>" + t("lang.label_#{locale}") + '</span></span>', params.merge(:locale => locale), :id => "switcher#{locale.capitalize}", :title => I18n.t("lang.switch_#{locale}")
+    end
+  end
+
 end
