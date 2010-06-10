bcc66afe13ab45ba8646130f364a8dc21f705256
Jonathan Meiss <jonathan@meiss.fr>
2010-06-10 14:10:33 +0200

    refactoring des applications controller en fonction des namespaces

--- app/controllers/client/pages_controller.rb
+++ app/controllers/client/pages_controller.rb
@@ -1,4 +1,4 @@
-class PagesController < ApplicationController
+class Client::PagesController < Client::ClientApplicationController
   def about
     
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.devise_for :users, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
 
-  map.resources :ads, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
-  map.resources :news, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
-  map.resources :users, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
-  map.resources :offers, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
-  map.resources :messages, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
-  map.resources :pictures, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
-  map.resources :housings, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
-  map.resources :bathrooms, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
-  map.resources :calendars, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
-  map.resources :reservations, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
-  map.resources :organizations, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
-  map.resources :message_usergroups, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
-  
-
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -46,8 +32,36 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
 
+  map.client :client do |client|
+    client.resources :ads, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
+    client.resources :news, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
+    client.resources :users, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
+    client.resources :offers, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
+    client.resources :messages, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
+    client.resources :pictures, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
+    client.resources :housings, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
+    client.resources :bathrooms, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
+    client.resources :calendars, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
+    client.resources :reservations, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
+    client.resources :organizations, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
+    client.resources :message_usergroups, :path_prefix => ":locale", :requirements => { :locale => /(fr)|(en)/ }
+  end
+
+  map.namespace :member do |member|
+    member.resources :ads
+  end
+  
+  map.namespace :admin do |admin|
+    admin.resources :ads
+  end
+
+
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "pages", :locale => "fr", :requirements => { :locale => /(fr)|(en)/ }
+  map.root :namespace => 'client', :controller => "pages", :locale => "fr", :requirements => { :locale => /(fr)|(en)/ }
+  
+  map.client 'client', :namespace => 'client', :controller => 'pages'
+  map.member 'member', :namespace => 'member', :controller => 'ads'
+  map.admin 'admin', :namespace => 'admin', :controller => 'ads'
 
   map.connect ":locale", :controller => "pages", :requirements => { :locale => /(fr)|(en)/ }
   # See how all your routes lay out with "rake routes"
